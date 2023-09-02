60cff4273d70a0c05e63fb509fd6d7e3c2c2c73c
Cristhian Vera <cristianmario092190@gmail.com>
2023-09-01 19:12:57 -0500

    view login completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hola");
+import { currentUser, tokenKey } from "./scripts/config.js";
+
+import DOMHandler from "./scripts/dom-handler.js";
+
+import { LoginPage } from "./scripts/pages/login-page.js";
+
+async function init() {
+  try {
+    const token = sessionStorage.getItem(tokenKey);
+    //console.log(token);
+    if (!token) return DOMHandler.load(LoginPage);
+    sessionStorage.getItem(currentUser);
+    //await dataTask.getTasks();
+    //dataTask.shortAlphabetic();
+    //console.log(dataTask);
+    //DOMHandler.load(HomePage);
+  } catch (error) {
+    console.log(error.message);
+    sessionStorage.removeItem(tokenKey);
+    sessionStorage.removeItem(currentUser);
+    DOMHandler.load(LoginPage);
+  }
+  //DOMHandler.reload();
+}
+init();
---
+++ scripts/pages/login-page.js
@@ -0,0 +1,83 @@
+import { header } from "../components/headers.js";
+import { input } from "../components/input.js";
+
+import DOMHandler from "../dom-handler.js";
+import { login } from "../services/sessions-services.js";
+
+function renderLogin() {
+  const { loginError } = LoginPage.state;
+  return `
+  ${header()}
+  <main class="section-sm">
+    <section class="container">      
+      <form class="flex flex-column gap-4 js-form-login">
+      ${loginError ? `<p class="danger">${loginError}</p>` : ""}
+      ${input({
+        label: "Email",
+        id: "txt_email",
+        placeholder: "you@example.com",
+        type: "email",
+        required: true,
+      })}
+
+      ${input({
+        label: "Password",
+        id: "txt_password",
+        type: "password",
+        required: true,
+      })}
+    
+      <button type="submit" class="button button--primary">Login</button>
+      <a href="#" class="text-center js-create-account">Create account</a>
+      </form>
+    </section>
+  </main>
+  `;
+}
+
+function listenSubmitLogin() {
+  const submitLogin = document.querySelector(".js-form-login");
+  submitLogin.addEventListener("submit", async (event) => {
+    try {
+      event.preventDefault();
+      const { txt_email, txt_password } = event.target.elements;
+      const formData = {
+        email: txt_email.value,
+        password: txt_password.value,
+      };
+      const user = await login(formData);
+      //dataTask.user = user;
+      await dataTask.getTasks();
+      dataTask.shortAlphabetic();
+      //console.log(dataTask);
+      //DOMHandler.load(HomePage);
+    } catch (error) {
+      //console.log(error);
+      LoginPage.state.loginError = error.message;
+    }
+    DOMHandler.reload();
+  });
+}
+
+function listenCreateAccount() {
+  const ancorCreateAccount = document.querySelector(".js-create-account");
+  ancorCreateAccount.addEventListener("click", (event) => {
+    event.preventDefault();
+    //DOMHandler.load(AccountPage);
+  });
+}
+
+const LoginPage = {
+  toString() {
+    return renderLogin();
+  },
+  addListeners() {
+    listenSubmitLogin();
+    listenCreateAccount();
+  },
+  state: {
+    loginError: null,
+  },
+};
+
+export { LoginPage };
